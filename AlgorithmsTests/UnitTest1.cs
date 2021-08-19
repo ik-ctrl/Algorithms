a5edcb0069088364483bf2cb02ee29afb2c2ec94
ikondratyev <kondratyev.ie@gmail.com>
2021-08-19 20:59:27 +0300

    добавил unit test

---
+++ AlgorithmsTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace AlgorithmsTests
+{
+    public class StupidSearchTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
