8313ea678aa1eff780b54b03eaf54d82cb1be56a
ikondratyev <kondratyev.ie@gmail.com>
2021-09-16 20:06:52 +0300

    Добавил проект для вспомогательных сущностей

--- HelpersEntities/Models/ProductType.cs
+++ HelpersEntities/Models/ProductType.cs
@@ -1,7 +1,19 @@
 ﻿namespace HelpersEntities
 {
+    /// <summary>
+    /// Тип продаваемых продуктов
+    /// </summary>
     public enum ProductType
     {
-        
+        Nothing,
+        Car,
+        Mango,
+        Apple,
+        House,
+        Wear,
+        Book,
+        Game,
+        Phone,
+        Computer
     }
 }
---
+++ HelpersEntities/Models/Seller.cs
@@ -0,0 +1,24 @@
+﻿namespace HelpersEntities
+{
+    /// <summary>
+    /// Сущность продавца
+    /// </summary>
+    public class Seller
+    {
+        /// <summary>
+        /// Тип продаваемой продукции
+        /// </summary>
+        public ProductType Type { get; set; }
+        
+        /// <summary>
+        /// Имя продавца
+        /// </summary>
+        public string Name { get; set; }
+        
+        /// <summary>
+        /// Был ли человек проверен алгоритмом
+        /// </summary>
+        public bool IsChecked { get; set; }
+        
+    }
+}
