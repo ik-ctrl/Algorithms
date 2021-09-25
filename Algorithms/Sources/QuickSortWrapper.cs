2bd364c2deb5bea5ba6b61b5e75815aa2aa10238
ikondratyev <kondratyev.ie@gmail.com>
2021-09-25 13:26:29 +0300

    реализаци обертки вокруг быстрой сортировки

--- Algorithms/Sources/QuickSortWrapper.cs
+++ Algorithms/Sources/QuickSortWrapper.cs
@@ -1,7 +1,78 @@
-﻿namespace Algorithms.Sources
+﻿using System.Diagnostics;
+
+namespace Algorithms.Sources
 {
-    public class QuickSortWrapper
+    /// <summary>
+    /// Обертка вокруг быстрой сортировки
+    /// </summary>
+    public class QuickSortWrapper:ISortAlgorithm,IAlgorithmStatistics
     {
+        private QuickSort _sorter;
+        private long _timeSpent;
+
+        public QuickSortWrapper()
+        {
+            _sorter = new QuickSort();
+        }
         
+        /// <summary>
+        /// Сортировка по возрастанию
+        /// </summary>
+        /// <param name="array">Сортируемый массив</param>
+        /// <returns>Отсортированный массив по возрастанию</returns>
+        /// <exception cref="ArgumentNullException"> array==null</exception>
+        public int[] SortAscending(int[] array)
+        {
+            var watch = new Stopwatch();
+            StartWatch(watch);
+            var result =_sorter.SortAscending(array);
+            StopWatch(watch);
+            return result;
+        }
+        
+        /// <summary>
+        /// Сортировка по убыванию
+        /// </summary>
+        /// <param name="array">Сортируемый массив</param>
+        /// <returns>Отсортированный массив по убыванию</returns>
+        /// <exception cref="ArgumentNullException"> array==null</exception>
+        public int[] SortDescending(int[] array)
+        {
+            var watch = new Stopwatch();
+            StartWatch(watch);
+            var result =_sorter.SortDescending(array);
+            StopWatch(watch);
+            return result;
+        }
+
+        /// <summary>
+        /// Времменные затраты
+        /// </summary>
+        public long TimeSpent => _timeSpent;
+
+        /// <summary>
+        /// Запуск таймера
+        /// </summary>
+        /// <param name="watch">Таймер</param>
+        private void StartWatch(Stopwatch watch) => watch?.Restart();
+
+        /// <summary>
+        /// Фиксация времени и остановка таймера
+        /// </summary>
+        /// <param name="watch"> таймер</param>
+        private void StopWatch(Stopwatch watch)
+        {
+            _timeSpent = (long) watch?.ElapsedMilliseconds;
+            watch?.Stop();
+        }
+        
+        /// <summary>
+        /// Сброс статистики
+        /// </summary>
+        public void Flush()
+        {
+            _timeSpent = default;
+            _sorter = new QuickSort();
+        }
     }
 }
--- AlgorithmsTests/QuickSortWrapperTest.cs
+++ AlgorithmsTests/QuickSortWrapperTest.cs
@@ -1,7 +1,141 @@
-﻿namespace AlgorithmsTests
+﻿using System;
+using System.Linq;
+using Algorithms.Sources;
+using NUnit.Framework;
+
+namespace AlgorithmsTests
 {
     public class QuickSortWrapperTest
     {
-        
+      
+        [TestCase(new int[] {5, 4, 3, 2, 1}, new int[] {1, 2, 3, 4, 5})]
+        [TestCase(new int[] {5, 4, 3, 1, 2}, new int[] {1, 2, 3, 4, 5})]
+        [TestCase(new int[] {5, 4, 3, 3, 1}, new int[] {1, 3, 3, 4, 5})]
+        [TestCase(new int[] {50, 544, 23, 32, 121}, new int[] {23, 32, 50, 121, 544})]
+        public void SortAscending_Test(int[] unsortedArray, int[] expectedArray)
+        {
+            var sorter = new QuickSortWrapper();
+            var newArray = sorter.SortAscending(unsortedArray);
+            for (int i = 0; i <= expectedArray.Length - 1; i++)
+            {
+                if (newArray[i] != expectedArray[i])
+                    Assert.Fail();
+            }
+
+            Assert.Pass();
+        }
+
+        [TestCase(10)]
+        [TestCase(20)]
+        [TestCase(30)]
+        [TestCase(100)]
+        [TestCase(200)]
+        [TestCase(300)]
+        [TestCase(500)]
+        [TestCase(1000)]
+        public void SortAscending_Random_Test(int arraySize)
+        {
+            var unsortedArray = GenerateRandomArray(arraySize);
+            var sortArray = (int[]) unsortedArray.Clone();
+            Array.Sort(sortArray);
+            var sorter = new QuickSortWrapper();
+            var newArray = sorter.SortAscending(unsortedArray);
+            for (int i = 0; i <= sortArray.Length - 1; i++)
+            {
+                if (newArray[i] != sortArray[i]) Assert.Fail();
+            }
+
+            Assert.Pass();
+        }
+
+
+        [TestCase(new int[] { }, new int[] { })]
+        public void SortAscending_EmptyArray_Test(int[] unsortedArray, int[] expectedArray)
+        {
+            var sorter = new QuickSortWrapper();
+            var newArray = sorter.SortAscending(unsortedArray);
+            Assert.True(newArray.Length == expectedArray.Length);
+        }
+
+        [TestCase(null, new int[] {1, 2, 3, 4, 5})]
+        public void SortAscending_Exception_Test(int[] unsortedArray, int[] expectedArray)
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var searcher = new QuickSortWrapper();
+                var index = searcher.SortAscending(unsortedArray);
+            });
+        }
+
+        [TestCase(new int[] {5, 4, 3, 2, 1}, new int[] {5, 4, 3, 2, 1})]
+        [TestCase(new int[] {5, 4, 3, 1, 2}, new int[] {5, 4, 3, 2, 1})]
+        [TestCase(new int[] {5, 4, 3, 3, 1}, new int[] {5, 4, 3, 3, 1})]
+        [TestCase(new int[] {50, 544, 23, 32, 121}, new int[] {544, 121, 50, 32, 23})]
+        public void SortDescending_Test(int[] unsortedArray, int[] expectedArray)
+        {
+            var sorter = new QuickSortWrapper();
+            var newArray = sorter.SortDescending(unsortedArray);
+            for (int i = 0; i <= expectedArray.Length - 1; i++)
+            {
+                if (newArray[i] != expectedArray[i])
+                    Assert.Fail();
+            }
+
+            Assert.Pass();
+        }
+
+        [TestCase(new int[] { }, new int[] { })]
+        public void SortDescending__EmptyArray_Test(int[] unsortedArray, int[] expectedArray)
+        {
+            var sorter = new QuickSortWrapper();
+            var newArray = sorter.SortDescending(unsortedArray);
+            Assert.True(newArray.Length == expectedArray.Length);
+        }
+
+        [TestCase(null, new int[] {1, 2, 3, 4, 5})]
+        public void SortDescending_Exception_Test(int[] unsortedArray, int[] expectedArray)
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var searcher = new QuickSortWrapper();
+                var index = searcher.SortDescending(unsortedArray);
+            });
+        }
+
+        [TestCase(10)]
+        [TestCase(20)]
+        [TestCase(30)]
+        [TestCase(100)]
+        [TestCase(200)]
+        [TestCase(300)]
+        [TestCase(500)]
+        [TestCase(1000)]
+        public void SortDescending_Random_Test(int arraySize)
+        {
+            var unsortedArray = GenerateRandomArray(arraySize);
+            var sortArray = (int[]) unsortedArray.Clone();
+            Array.Sort(sortArray);
+            Array.Reverse(sortArray);
+            var sorter = new QuickSortWrapper();
+            var newArray = sorter.SortDescending(unsortedArray);
+            for (int i = 0; i <= sortArray.Length - 1; i++)
+            {
+                if (newArray[i] != sortArray[i]) Assert.Fail();
+            }
+
+            Assert.Pass();
+        }
+
+
+        #region helpers
+
+        private int[] GenerateRandomArray(int arraySize)
+        {
+            var randomizer = new Random(DateTime.Now.Millisecond & 0xf0f0f0);
+            return Enumerable.Range(0, arraySize).Select(x => randomizer.Next(-1000, 1000)).ToArray();
+        }
+
+        #endregion
     }
+    
 }
