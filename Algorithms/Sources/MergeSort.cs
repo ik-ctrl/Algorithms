b71dd7dd6e307019e1fa9a9180e8e393383b68b0
ikondratyev <kondratyev.ie@gmail.com>
2022-02-10 21:49:57 +0300

    Добавил слияние двух массивов

---
+++ Algorithms/Sources/MergeSort.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Algorithms.Sources
+{
+    /// <summary>
+    /// Сортировка слиянием O(nlogn)
+    /// </summary>
+    public static class MergeSort
+    {
+        /// <summary>
+        /// Сортировка по убыванию
+        /// </summary>
+        /// <param name="firstOrderedArray">Первый массив отсортированный по убыванию</param>
+        /// <param name="secondOrderedArray">Второй массив отсортированный по убыванию</param>
+        /// <returns>Убыванию массив по возрастанию</returns>
+        /// <exception cref="ArgumentException">Один из массивов равен null</exception>
+        public static int[] SortDescending (int[] firstOrderedArray, int[] secondOrderedArray)
+        {
+            if (firstOrderedArray == null || secondOrderedArray == null)
+                throw new ArgumentException("Один из массивов равен null");
+
+            var i = 0;
+            var j = 0;
+            var k = 0;
+            var resultArray = new int[firstOrderedArray.Length + secondOrderedArray.Length];
+            while (i < firstOrderedArray.Length && j < secondOrderedArray.Length)
+            {
+                resultArray[k] = firstOrderedArray[i] >secondOrderedArray[j]
+                    ? firstOrderedArray[i++]
+                    : secondOrderedArray[j++];
+                    k++;
+            }
+            
+            while (i < firstOrderedArray.Length)
+            {
+                resultArray[k] = firstOrderedArray[i++];
+                k++;
+            }
+            
+            while (j < secondOrderedArray.Length)
+            {
+                resultArray[k] = secondOrderedArray[j++];
+                k++;
+            }
+            return resultArray;
+        }
+
+        /// <summary>
+        /// Сортировка по возрастанию
+        /// </summary>
+        /// <param name="firstOrderedArray">Первый массив отсортированный по возрастанию</param>
+        /// <param name="secondOrderedArray">Второй массив отсортированный по возрастанию</param>
+        /// <returns>Отсортированный массив по возрастанию</returns>
+        /// <exception cref="ArgumentException">Один из массивов равен null</exception>
+        public static int[] SortAscending(int[] firstOrderedArray, int[] secondOrderedArray)
+        {
+            if (firstOrderedArray == null || secondOrderedArray == null)
+                throw new ArgumentException("Один из массивов равен null");
+
+            var i = 0;
+            var j = 0;
+            var k = 0;
+            var resultArray = new int[firstOrderedArray.Length + secondOrderedArray.Length];
+            while (i < firstOrderedArray.Length && j < secondOrderedArray.Length)
+            {
+                resultArray[k] = firstOrderedArray[i] < secondOrderedArray[j]
+                    ? firstOrderedArray[i++]
+                    : secondOrderedArray[j++];
+                k++;
+            }
+            
+            while (i < firstOrderedArray.Length)
+            {
+                resultArray[k] = firstOrderedArray[i++];
+                k++;
+            }
+            
+            while (j < secondOrderedArray.Length)
+            {
+                resultArray[k] = secondOrderedArray[j++];
+                k++;
+            }
+            return resultArray;
+        }
+
+        public static int[] ArraySort(int[] array)
+        {
+            
+        }
+    }
+}
---
+++ AlgorithmsTests/MergeSortTests.cs
@@ -0,0 +1,35 @@
+﻿using Algorithms.Sources;
+using NUnit.Framework;
+
+namespace AlgorithmsTests
+{
+    [TestFixture]
+    public class MergeSortTests
+    {
+        [Test]
+        public void SortDescending_Test()
+        {
+            var first = new int[] { 7, 5, 4, 3, 2, 1 };
+            var second = new int[] { 10, 9, 8, 6 };
+            var expected = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            var result = MergeSort.SortDescending(first, second);
+            for (var i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], result[i]);
+            }
+        }
+
+        [Test]
+        public void SortAscending_Test()
+        {
+            var first = new int[] { 1, 2, 3, 4, 5, 7 };
+            var second = new int[] { 6, 8, 9, 10 };
+            var expected = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var result = MergeSort.SortAscending(first, second);
+            for (var i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], result[i]);
+            }
+        }
+    }
+}
